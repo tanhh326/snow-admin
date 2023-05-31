08c00abdd9ef9d1a8a80849923908d7c95c609b3
🌩 <hehong_tan@xmemtech.com>
2023-05-31 21:54:55 +0800

    fix: 改为计算属性

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,15 +3,18 @@
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 // @ts-ignore
 import en from 'element-plus/dist/locale/en.mjs';
+import { computed } from 'vue';
 
 const elementPlusLanguage: { [key: string]: any } = {
   zhCn,
   en,
 };
+
+const language = computed(() => navigator.language);
 </script>
 
 <template>
-  <el-config-provider :locale="elementPlusLanguage[navigator.language]">
+  <el-config-provider :locale="elementPlusLanguage[language]">
     <router-view />
   </el-config-provider>
 </template>
